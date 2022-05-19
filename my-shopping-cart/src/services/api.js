eb74f9e1630cb47a26d1db17bb125d2886d87c30
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-05-19 19:59:06 -0300

    list products from api with name, image and price

---
+++ my-shopping-cart/src/services/api.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from "axios";
+
+const baseURL = 'https://fakestoreapi.com/products';
+
+export default function Api() {
+  const [products, setProducts] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setProducts(response.data);
+    });
+  }, [])
+  return products
+}
