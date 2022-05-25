f83287042434d6f0876cc241761ab984e89dc790
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-05-25 17:35:31 -0300

    add cart state

--- my-shopping-cart/src/pages/ListProducts.js
+++ my-shopping-cart/src/pages/ListProducts.js
@@ -3,6 +3,7 @@ import useApi from '../services/api';
 
 export default function ListProducts() {
   const products = useApi();
+  const [cartProducts, setCartProducts] = React.useState([]);
   return (
     <section>
       {products === null ? <h1>carregando...</h1> :
@@ -11,6 +12,17 @@ export default function ListProducts() {
             <h1>{title}</h1>
             <img src={image} alt={title} />
             <h2>{price}</h2>
+            <button
+              onClick={() => {
+                setCartProducts([...cartProducts,
+                {
+                  id: id,
+                  title: title,
+                  price: price,
+                  image: image
+                }])
+              }}
+            >add to cart</button>
           </div>
         )}
     </section>
