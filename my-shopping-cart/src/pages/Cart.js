f0bdc97f2f1f95e7a986bc83733bfc6137a33d42
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-05-25 18:37:05 -0300

    add cart

--- my-shopping-cart/src/pages/Cart.js
+++ my-shopping-cart/src/pages/Cart.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import MyContext from '../context/Context';
+import { useNavigate } from "react-router-dom";
 
 export default function Cart() {
+  const { cartProducts } = React.useContext(MyContext);
+  const navigate = useNavigate();
   return (
-    <div>Bem vindo ao carrinho de compras</div>
+    <section>
+      <h1>Bem vindo ao carrinho de compras</h1>
+      <button onClick={() => navigate("/")}>voltar as compras!</button>
+      <section>
+        {cartProducts === null ? <h1>carregando...</h1> :
+          cartProducts.map(({ image, title, price, id }) =>
+            <div key={id}>
+              <h1>{title}</h1>
+              <img src={image} alt={title} />
+              <h2>{price}</h2>
+            </div>
+          )}
+      </section>
+    </section>
   )
 }
