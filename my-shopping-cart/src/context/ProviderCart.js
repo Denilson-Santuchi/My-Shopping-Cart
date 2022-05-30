058d3e131aa2c6fb6cc1f1b597702923ffbbaea6
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-05-30 15:55:57 -0300

    loding spineer 3/8

--- my-shopping-cart/src/context/ProviderCart.js
+++ my-shopping-cart/src/context/ProviderCart.js
@@ -3,8 +3,15 @@ import MyContext from './Context'
 
 export default function ProviderCart({ children }) {
   const [cartProducts, setCartProducts] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
   return (
-    <MyContext.Provider value={{ cartProducts, setCartProducts }}>
+    <MyContext.Provider value={{
+      cartProducts,
+      setCartProducts,
+      isLoading,
+      setIsLoading
+      }}>
       {children}
     </MyContext.Provider>
   )
